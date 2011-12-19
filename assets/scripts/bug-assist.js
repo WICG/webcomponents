81cdcc966963e2542a65bf0b76909b52d44e11b0
Dimitri Glazkov <dglazkov@google.com>
2011-12-19 11:14:34 -0800

    15014: [bug-assist]: Abstract out bug settings. https://www.w3.org/Bugs/Public/show_bug.cgi?id=15014

--- assets/scripts/bug-assist.js
+++ assets/scripts/bug-assist.js
@@ -1,11 +1,35 @@
+/*
+
+Simple Bug File Assistant.
+Find bugs? File a bug, blocking https://www.w3.org/Bugs/Public/show_bug.cgi?id=15013.
+To configure, use meta tags as follows:
+
+* The content of each meta tag whose name starts with "bug." becomes a form parameter for Bugzilla new bug submission.
+* The name "bug.blocked" is special. If present, the assistant add a link to the dependency tree of this bug.
+* The name "bug.comment" is overriden if the user selects text.
+
+Sample use and configuration:
+
+<script src="../assets/scripts/bug-assist.js"></script>
+<script src="../assets/scripts/prettify.js"></script>
+<meta name="bug.blocked" content="14949">
+<meta name="bug.short_desc" content="[Explainer]: ">
+<meta name="bug.product" content="WebAppsWG">
+<meta name="bug.component" content="Component Model">
+
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
-    var inputs = {
-            product: 'WebAppsWG',
-            component: 'Component Model',
-            short_desc: '[Explainer]: ',
-            comment: '',
-            blocked: '14949'
-    };
+    var BUGS_PREFIX = 'bug.';
+
+    var inputs = {  comment: '' };
+
+    [].forEach.call(document.querySelectorAll('meta'), function(meta) {
+        if (meta.name.indexOf(BUGS_PREFIX) == 0)
+            inputs[meta.name.substr(BUGS_PREFIX.length)] = meta.content;
+    });
+
+    var blocked = inputs.blocked;
 
     var form = document.body.appendChild(document.createElement('form'));
     form.style.cssText = 'position:fixed;width:10em;top:1em;right:1em;font-family:Tahoma,sans-serif;font-size:11px;opacity:0.8;text-align:right';
@@ -30,11 +54,13 @@ document.addEventListener('DOMContentLoaded', function() {
         inputs[name] = input;
     });
 
-    form.appendChild(document.createTextNode(' or '));
-    var a = form.appendChild(document.createElement('a'));
-    a.textContent = 'view bugs filed';
-    a.href = 'http://www.w3.org/Bugs/Public/showdependencytree.cgi?id=14949';
-    a.target = '_blank';
+    if (blocked) {
+        form.appendChild(document.createTextNode(' or '));
+        var a = form.appendChild(document.createElement('a'));
+        a.textContent = 'view bugs filed';
+        a.href = 'http://www.w3.org/Bugs/Public/showdependencytree.cgi?id=' + blocked;
+        a.target = '_blank';
+    }
     form.appendChild(document.createTextNode('.'));
 
     form.addEventListener('submit', function() {
