a8735ecf0094351f5fa45734234b404e299fec13
Dimitri Glazkov <dglazkov@chromium.org>
2013-05-07 12:56:57 -0700

    Make spec-assist look up the date from the change log.

--- assets/scripts/spec-assist.js
+++ assets/scripts/spec-assist.js
@@ -1,8 +1,43 @@
+(function() {
+
+function fetchLastUpdated(callback)
+{
+    var lastUpdated = new Date();
+    var logAnchorNode = document.querySelector('a#log');
+    if (!logAnchorNode)
+        return callback(lastUpdated);
+
+    var logURL = logAnchorNode.href.replace("/log", "/log-atom");
+    if (!logURL)
+        return callback(lastUpdated);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        var doc = this.responseXML;
+        if (!doc)
+            return callback(lastUpdated);
+        var updatedNode = doc.querySelector('feed>updated');
+        if (!updatedNode)
+            return callback(lastUpdated);
+        callback(Date.parse(updatedNode.textContent));
+    }
+    xhr.onerror = function() {
+        callback(lastUpdated);
+    }
+    xhr.open('GET', logURL);
+    xhr.send();
+}
+
+function prettyDate(date) {
+    return ' ' + date.getDate() + ' ' + ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()] + ' ' + date.getFullYear();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var title = document.querySelector('h2#editors-draft');
     if (title) {
-        var date = new Date();
-        title.appendChild(document.createTextNode(' ' + date.getDate() + ' ' + ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()] + ' ' + date.getFullYear()));
+        fetchLastUpdated(function(date) {
+            title.appendChild(document.createTextNode(prettyDate(date)));
+        });
     }
     [].forEach.call(document.querySelectorAll('section.toc li span.section'), function(sectionNumber) {
         var href = sectionNumber.parentElement.getAttribute('href');
@@ -15,4 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
     [].forEach.call(document.querySelectorAll('dfn[id]'), function(definition) {
         definition.setAttribute('title', '#' + definition.id);
     });
-})
+});
+
+}());
